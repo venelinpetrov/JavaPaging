bbded88dc6caf0beadfd85d653f00c7666e2ddd0
venelinpetrov <venelin.petrov@cubbystorage.com>
2023-10-03 15:21:08 +0300

    Add faker

--- src/main/java/dev/vpetrov/paging/SampleDataLoader.java
+++ src/main/java/dev/vpetrov/paging/SampleDataLoader.java
@@ -1,6 +1,11 @@
 package dev.vpetrov.paging;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.github.javafaker.Faker;
 
 import dev.vpetrov.paging.model.Address;
 import dev.vpetrov.paging.model.Person;
@@ -9,15 +14,29 @@ import dev.vpetrov.paging.repository.PersonRepository;
 @Component
 public class SampleDataLoader implements CommandLineRunner {
     private final PersonRepository repository;
+    private final Faker faker;
     public SampleDataLoader(PersonRepository repository) {
         this.repository = repository;
+        this.faker = new Faker();
     }
 
     @Override
     public void run(String ...args) {
-        Person p = new Person("josh", "smith", "123", "j.s@mail.com", new Address("1 str", "Sofia", "Sofia", "1000"));
+        List<Person> people = IntStream.rangeClosed(1, 100)
+            .mapToObj(i -> new Person(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.phoneNumber().cellPhone(),
+                faker.internet().emailAddress(),
+                new Address(
+                    faker.address().streetAddress(),
+                    faker.address().city(),
+                    faker.address().state(),
+                    faker.address().zipCode()
+                )
+            )).toList();
 
-        repository.save(p);
+        repository.saveAll(people);
     }
 
 }
