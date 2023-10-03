54f8f71bf2a4278c86fb6357ecf7701c4c20331b
venelinpetrov <venelin.petrov@cubbystorage.com>
2023-10-03 11:49:56 +0300

    Initial commit

---
+++ src/test/java/dev/vpetrov/paging/PagingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vpetrov.paging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
